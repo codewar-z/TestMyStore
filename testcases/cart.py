fb59f444ecfd8ba812761b83fcd1127f6c27ad34
Jyothy Mariam John <jyothym@gmail.com>
2019-03-22 16:33:51 +0530

    Jyothy | refined cart.py

--- testcases/cart.py
+++ testcases/cart.py
@@ -1,7 +1,6 @@
 import unittest
 import objects.locators as locator
 import time
-import random
 from selenium.webdriver import Firefox, Chrome
 import config.params as params
 
@@ -16,93 +15,34 @@ class TestCart(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.browser = browserList.get(params.browser)()
-        cls.browser.set_window_size(1024, 1366)
-        time.sleep(5)
         cls.browser.get(params.url)
 
         cls.browser.find_element_by_css_selector(locator.signin).click()
-    #     cls.browser.find_element_by_id(locator.email).send_keys(params.username)
-    #     cls.browser.find_element_by_id(locator.password).send_keys(params.password)
-    #     cls.browser.find_element_by_id(locator.submit).click()
-    #
-    # def test_ValidLogin(self):
-    #     self.assertEqual(self.browser.find_element_by_css_selector(locator.account).text, 'Jyothy Mariam John')
-    #
-    # def test_EmptyCart(self):
-    #     self.assertEqual(self.browser.find_element_by_css_selector(locator.cart_empty).text, '')
-    #
-    # def test_AddToCart(self):
-    #     self.browser.find_element_by_id('search_query_top').send_keys('Faded Short Sleeve T-shirts')
-    #     self.browser.find_element_by_name('submit_search').click()
-    #     self.browser.find_element_by_css_selector('.icon-th-list').click()
-    #     self.browser.find_element_by_css_selector('.ajax_add_to_cart_button').click()
-    #     time.sleep(5)
-    #     self.browser.find_element_by_css_selector('.continue').click()
-    #     self.assertEqual(self.browser.find_element_by_css_selector(locator.cart_quantity).text, '1')
+        cls.browser.find_element_by_id(locator.email).send_keys(params.username)
+        cls.browser.find_element_by_id(locator.password).send_keys(params.password)
+        cls.browser.find_element_by_id(locator.submit).click()
 
-    # def test_Registration(self):
-    #     userRegistration = RegisterUserWithOnlyMandatoryFields("TestUser", "Users", "random300@gmail.com", "anything")
-    #     userRegistration.register(self.browser)
+    def test_ValidLogin(self):
+        self.assertEqual(self.browser.find_element_by_css_selector(locator.account).text, 'Jyothy Mariam John')
 
-    def test_Registration(self):
-        reg_email = f'random{random.randint(1,1000)}@gmail.com'
-        user_registration = RegisterUserWithOnlyMandatoryFields("TestUser", "Users", reg_email, "anything")
-        user_registration.register(self.browser)
-        self.browser.find_element_by_id(locator.submit_register).click()
+    def test_EmptyCart(self):
+        self.assertEqual(self.browser.find_element_by_css_selector(locator.cart_empty).text, '')
 
-    def test_Registration_with_additional_fields(self):
-        reg_email = f'random{random.randint(1,1000)}@gmail.com'
-        user_registration = RegisterUserWithAdditionalFields("TestUser", "Users", reg_email, "anything")
-        user_registration.register(self.browser)
-        self.browser.find_element_by_id(locator.submit_register).click()
-
-
-
-    # @classmethod
-    # def tearDownClass(cls):
-    #     cls.browser.find_element_by_css_selector(locator.logout).click()
-    #     cls.browser.close()
-    #     cls.browser.quit()
-
-
-class RegisterUserWithOnlyMandatoryFields(object):
-    def __init__(self, first_name, last_name, email, password):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.email = email
-        self.password = password
-        self.address = '6th Main'
-        self.city = 'xyz'
-        self.state = 'Indiana'
-        self.zip = '46204'
-        self.country = 'United States'
-        self.mobile = '9192634580'
-        self.alias = 'test account'
-
-    def register(self, browser):
-        browser.find_element_by_id(locator.email).send_keys(self.email)
-        browser.find_element_by_id(locator.submit_create).click()
+    def test_AddToCart(self):
+        self.browser.find_element_by_id('search_query_top').send_keys('Faded Short Sleeve T-shirts')
+        self.browser.find_element_by_name('submit_search').click()
+        self.browser.find_element_by_css_selector('.icon-th-list').click()
+        self.browser.find_element_by_css_selector('.ajax_add_to_cart_button').click()
         time.sleep(5)
-        browser.find_element_by_id(locator.first_name).send_keys(self.first_name)
-        browser.find_element_by_id(locator.last_name).send_keys(self.last_name)
-        browser.find_element_by_id(locator.password).send_keys(self.password)
-        browser.find_element_by_id(locator.address).send_keys(self.address)
-        browser.find_element_by_id(locator.city).send_keys(self.city)
-        browser.find_element_by_id(locator.state).send_keys(self.state)
-        browser.find_element_by_id(locator.zip).send_keys(self.zip)
-        browser.find_element_by_id(locator.country).send_keys(self.country)
-        browser.find_element_by_id(locator.mobile).send_keys(self.mobile)
+        self.browser.find_element_by_css_selector('.continue').click()
+        self.assertEqual(self.browser.find_element_by_css_selector(locator.cart_quantity).text, '1')
 
+    @classmethod
+    def tearDownClass(cls):
+        cls.browser.find_element_by_css_selector(locator.logout).click()
+        cls.browser.close()
+        cls.browser.quit()
 
-class RegisterUserWithAdditionalFields(RegisterUserWithOnlyMandatoryFields):
-    def __init__(self,first_name, last_name, email, password):
-        self.company = 'Techno Logic'
-        RegisterUserWithOnlyMandatoryFields.__init__(self, first_name, last_name, email, password)
-
-    def register(self, browser):
-        RegisterUserWithOnlyMandatoryFields.register(self, browser)
-        browser.find_element_by_id(locator.company).send_keys(self.company)
-        time.sleep(15)
 
 if __name__ == '__main__':
     unittest.main()
